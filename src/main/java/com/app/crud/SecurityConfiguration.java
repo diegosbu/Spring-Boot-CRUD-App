de0a63e6dd64c7c7589f17b5d882f308d84f46d0
Diego Sanchez <diegos@bu.edu>
2022-07-27 20:00:59 -0400

    Implemented reusable thymeleaf fragments + thymeleaf extras spring security

--- src/main/java/com/app/crud/SecurityConfiguration.java
+++ src/main/java/com/app/crud/SecurityConfiguration.java
@@ -8,10 +8,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfiguration {
@@ -32,7 +32,8 @@ public class SecurityConfiguration {
                 .antMatchers("/logout").authenticated()
                 .antMatchers("/login*").anonymous()
                 .antMatchers("/register*").anonymous()
-                .antMatchers("/").permitAll()
+                .antMatchers("/", "/resources/**", "/static/**", "/css/**", "/js/**", "/images/**",
+                        "/vendor/**","/fonts/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
